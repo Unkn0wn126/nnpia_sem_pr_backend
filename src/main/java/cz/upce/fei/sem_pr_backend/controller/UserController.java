4aebe0f3ebcd95dfee06f31c9aa4003fd246e3cb
Unkn0wn126 <32388228+Unkn0wn126@users.noreply.github.com>
2022-04-10 15:00:13 +0200

    Changed signature in UserController

--- src/main/java/cz/upce/fei/sem_pr_backend/controller/UserController.java
+++ src/main/java/cz/upce/fei/sem_pr_backend/controller/UserController.java
@@ -1,33 +1,44 @@
 package cz.upce.fei.sem_pr_backend.controller;
 
+import cz.upce.fei.sem_pr_backend.dto.applicationuser.ApplicationUserGetDto;
+import cz.upce.fei.sem_pr_backend.service.ApplicationUserService;
 import org.springframework.web.bind.annotation.*;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    private final ApplicationUserService userService;
+
+    public UserController(ApplicationUserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
-    public void getUsers(){
-        System.out.println("users");
+    public List<ApplicationUserGetDto> getUsers(){
+        throw new NotImplementedException(); // TODO
     }
 
     @GetMapping("/id/{id}")
-    public void getUserById(@PathVariable Long id){
-        System.out.println("user with id " + id);
+    public ApplicationUserGetDto getUserById(@PathVariable Long id){
+        throw new NotImplementedException(); // TODO
     }
 
     @GetMapping("/{username}")
-    public void getIssueByUsername(@PathVariable String username){
-        System.out.println("user with name " + username);
+    public ApplicationUserGetDto getUserByUsername(@PathVariable String username){
+        throw new NotImplementedException(); // TODO
     }
 
     @DeleteMapping("/delete/{id}")
     public void deleteUserById(@PathVariable Long id){
-        System.out.println("delete user with id " + id);
+        throw new NotImplementedException(); // TODO
     }
 
     @PutMapping("/update/{id}")
     public void updateUserById(@PathVariable Long id){
-        System.out.println("update user with id " + id);
+        throw new NotImplementedException(); // TODO
     }
 }
