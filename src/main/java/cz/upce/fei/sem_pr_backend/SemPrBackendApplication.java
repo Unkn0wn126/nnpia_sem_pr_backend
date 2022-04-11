cec7212a9090a2ce1fa2712de28c2d2a4b6ae1db
Unkn0wn126 <32388228+Unkn0wn126@users.noreply.github.com>
2022-04-11 18:47:52 +0200

    Added configurations for production

--- src/main/java/cz/upce/fei/sem_pr_backend/SemPrBackendApplication.java
+++ src/main/java/cz/upce/fei/sem_pr_backend/SemPrBackendApplication.java
@@ -23,12 +23,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.HashSet;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SemPrBackendApplication {
 
     public static void main(String[] args) {
